683aab7f33ec86353bbac34b6827d204cd4255ae
Hideaki SAWADA <chuno@live.jp>
2014-09-14 06:53:13 +0900

    Create settings.php

---
+++ lang/ja/settings.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+
+$lang['admin_only'] = '管理者（スーパーユーザー）だけにタイムスタンプ更新を許可する。';
