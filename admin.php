c03bf2884f7172286f2c7d372be408958c87b6d8
Rainbow Spike <Dr-Yukon@users.noreply.github.com>
2019-11-20 01:11:55 +1000

    Lang variables

--- admin.php
+++ admin.php
@@ -7,46 +7,44 @@
  */
 
 // must be run within Dokuwiki
-if (!defined('DOKU_INC')) die();
+if ( !defined ( 'DOKU_INC' ) ) die ( );
 
-if (!defined('DOKU_LF')) define('DOKU_LF', "\n");
-if (!defined('DOKU_TAB')) define('DOKU_TAB', "\t");
-if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+if ( !defined ( 'DOKU_LF' ) ) define ( 'DOKU_LF', "\n" );
+if ( !defined ( 'DOKU_TAB' ) ) define ( 'DOKU_TAB', "\t" );
+if ( !defined ( 'DOKU_PLUGIN' ) ) define ( 'DOKU_PLUGIN', DOKU_INC . 'lib/plugins/' );
 
-require_once DOKU_PLUGIN.'admin.php';
+require_once DOKU_PLUGIN . 'admin.php';
 
 class admin_plugin_toucher extends DokuWiki_Admin_Plugin {
 
-    public function getMenuSort() {
+    public function getMenuSort ( ) {
         return 13;
     }
 
-    public function forAdminOnly() {
-        return $this->getConf('admin_only');
+    public function forAdminOnly ( ) {
+        return $this -> getConf ( 'admin_only' );
     }
 
-    function touchFiles() {
-        touch(DOKU_CONF."local.php"); // this is the core of this plugin
+    function touchFiles ( ) {
+        touch ( DOKU_CONF . "local.php" ); // this is the core of this plugin
     }
  
-    public function handle() {
+    public function handle ( ) {
         global $INFO;
 
-        if ($this->getConf('admin_only')) {
-            if (!$INFO[isadmin]) {
-                msg('Plugin toucher failed: you must be admin to touch configuration',-1);
+        if ( $this -> getConf ( 'admin_only' ) ) {
+            if ( !$INFO [ 'isadmin' ] ) {
+                msg ( $this -> getLang ( 'denied' ), -1);
                 return false;
             }
         }
-        $this->touchFiles();
-        msg('Plugin toucher touched configuration files',1);
+        $this -> touchFiles ( );
+        msg ( $this -> getLang ( 'success' ), 1);
         return true;
     }
 
-    public function html() {
-        ptln('<h1>' . $this->getLang('menu') . '</h1>');
-        ptln('<p>Configuration files have been just touched.</p>');
+    public function html ( ) {
+        ptln ( '<h1>' . $this -> getLang ( 'menu' ) . '</h1>' );
+        ptln ( $this -> getLang ( 'success_p' ) );
     }
 }
-
-// vim:ts=4:sw=4:et:
